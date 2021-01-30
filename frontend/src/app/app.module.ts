784b7f639d0b432958f277b35a6ebde7d5165e9e
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-01-30 23:37:37 +0530

    Frontend: Added UI for SignIn and SignUp

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { AuthComponent } from './auth/auth.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.3s ease-out',
+              style({ height: 76, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 76, opacity: 1 }),
+            animate('0.3s ease-in',
+              style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ],
+    ),
+  ],
 })
 export class AuthComponent implements OnInit {
+  name = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  showPassword = false;
+
+  authType: 'REGISTER' | 'LOGIN' = 'LOGIN';
 
   constructor() { }
 
