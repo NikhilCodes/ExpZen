7c52b8798e1871357fcd050309d8df8797f251b1
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-03-01 21:45:38 +0530

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // serverUrl: 'http://localhost:8000/api'
-  serverUrl: 'https://067446da2719.ngrok.io/api'
+  serverUrl: 'https://92ac09d4766c.ngrok.io/api'
 };
