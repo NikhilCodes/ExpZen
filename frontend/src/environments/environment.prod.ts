6a023c76fb7ddf22a7e7224bb7b7c82e2a968534
Nikhil Nayak <nikhil.nixel@gmail.com>
2021-03-01 21:20:55 +0530

    Url fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // serverUrl: 'http://localhost:8000/api'
-  serverUrl: 'https://067446da2719.ngrok.io'
+  serverUrl: 'https://067446da2719.ngrok.io/api'
 };
